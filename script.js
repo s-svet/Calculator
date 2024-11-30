b174edfe175f243e15c10cc17e9a5edf3af125de
s-svet <155708423+s-svet@users.noreply.github.com>
2024-11-29 20:52:01 -0800

    Add math and operate functions

---
+++ script.js
@@ -0,0 +1,31 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(firstNumber, operator, secondNumber) {
+    if (operator === "+") {
+        return add(firstNumber, secondNumber);
+    } else if (operator === "-") {
+        return subtract(firstNumber, secondNumber);
+    } else if (operator === "*") {
+        return multiply(firstNumber, secondNumber);
+    } else if (operator === "/") {
+        return divide(firstNumber, secondNumber);
+    }
+}
